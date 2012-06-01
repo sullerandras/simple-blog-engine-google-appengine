44c7c54fd2d7f2c2b3652808c9e058b22e51ea1b
Andras Suller <suller.andras@gmail.com>
2012-06-01 20:30:01 +0800

    lettuce tests for scenario: visit home page with no blog entries
    Ignore-this: 6c3eb7477452dbc73c77b318c9e33227
    
    darcs-hash:20120601123001-5fa3b-24693bac476c7a7959818367055b0e20d383dad3

---
+++ features/steps.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from lettuce import step, world
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+@step(u'Given there are no blog entries in the database')
+def given_there_are_no_blog_entries_in_the_database(step):
+    pass
+
+@step(u'When I visit the home page')
+def when_i_visit_the_home_page(step):
+    world.browser = webdriver.Firefox() # Get local session of firefox
+    world.browser.get("http://localhost:8080") # Load page
+
+@step(u'Then I should see the message "([^"]*)"')
+def then_i_should_see_the_message(step, msg):
+    body = world.browser.find_element_by_xpath("//body")
+    assert body.text == msg, "I should see '%s', but I see '%s'" % (msg, body.text)
