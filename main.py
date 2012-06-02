0ccfc81e38ba9dea4d852033ea0762a49c802aa7
Andras Suller <suller.andras@gmail.com>
2012-06-02 11:31:39 +0800

    reindent source; move the get method into the base class
    Ignore-this: 4ee405acd73a6440090f140b27c66249
    
    darcs-hash:20120602033139-5fa3b-fa80691ce7b050725090d9f4e6194b793fc7cc41

--- main.py
+++ main.py
@@ -16,37 +16,34 @@ class MockRequest(object):
         self.host = 'localhost:8080'
 
 class BaseRequestHandler(webapp.RequestHandler):
-  """Supplies a common template generation function.
+    """Supplies a common template generation function.
 
-  When you call renderTemplate(), we augment the template variables supplied with
-  the current user in the 'user' variable and the current webapp request
-  in the 'request' variable.
-  """
-  def renderTemplate(self, template_name, template_values={}):
-    req = self.request if hasattr(self, 'request') else MockRequest()
-    values = {
-        'request': req,
-        'user': users.get_current_user(),
-        'is_admin': users.is_current_user_admin(),
-        'login_url': users.create_login_url(req.uri),
-        'logout_url': users.create_logout_url('http://%s/' % (req.host,))
-        }
-    values.update(template_values)
-    directory = os.path.dirname(__file__)
-    path = os.path.join(directory, os.path.join('templates', template_name))
-    return template.render(path, values, debug=_DEBUG)
+    When you call renderTemplate(), we augment the template variables supplied with
+    the current user in the 'user' variable and the current webapp request
+    in the 'request' variable.
+    """
+    def renderTemplate(self, template_name, template_values={}):
+        req = self.request if hasattr(self, 'request') else MockRequest()
+        values = {
+            'request': req,
+            'user': users.get_current_user(),
+            'is_admin': users.is_current_user_admin(),
+            'login_url': users.create_login_url(req.uri),
+            'logout_url': users.create_logout_url('http://%s/' % (req.host,))
+            }
+        values.update(template_values)
+        directory = os.path.dirname(__file__)
+        path = os.path.join(directory, os.path.join('templates', template_name))
+        return template.render(path, values, debug=_DEBUG)
 
-class IndexHandler(BaseRequestHandler):
     def get(self):
         self.response.out.write(self.render())
 
+class IndexHandler(BaseRequestHandler):
     def render(self):
         return self.renderTemplate('index.html')
 
 class NewBlogEntryHandler(BaseRequestHandler):
-    def get(self):
-        self.response.out.write(self.render())
-
     def render(self):
         return self.renderTemplate('new-blog-entry.html')
 
