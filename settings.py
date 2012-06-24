da583f76327c27447d6badc0ca1b32b3177ff85b
Andras Suller <suller.andras@gmail.com>
2012-06-24 15:03:11 +0800

    add additional comment about using the blog engine as a submodule
    Ignore-this: df5cd5ef30abd02bf0e336aa4980ba9e
    
    darcs-hash:20120624070311-5fa3b-64c8f1f4a1ac1b2f0dfcddf7cfc1d0d7a196d6ea

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # set this to "/blog", if you want to publish the blog engine on the http://<your domain>/blog url.
 # it is useful if you want to use one domain for several services, like "/wiki", "/blog", "/todo", etc.
+# if you want to use a different name than "blog", modify the import in main.py as well.
 BASE = '/blog'
 
 # the application's public URLs
